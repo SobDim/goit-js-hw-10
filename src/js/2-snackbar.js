1acb86a1605c8e98a0ebc2527f3d8104a68a8ea5
SobDim <101928077+SobDim@users.noreply.github.com>
2024-03-07 22:57:00 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,20 +4,38 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.js-form');
-let delay;
+// let delay;
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
-  e.preventDefault;
-  delay = e.currentTarget.elements.value;
+  e.preventDefault();
+  const delay = e.currentTarget.elements.delay.value;
+
+  console.log(e);
 }
-console.log(delay);
 // `✅ Fulfilled promise in ${delay}ms`;
 // `❌ Rejected promise in ${delay}ms`;
 
-// function createPromise(delay, callback) {
-//   return new Promise(res => {
-//     setTimeout(() => res, delay);
-//   }).then(() => callback);
+function createPromise(delay, callback) {
+  return new Promise((res, rej) => {
+    setTimeout(() => res, delay);
+  }).then(() => callback);
+}
+// function countWithDelay(delay, times, interval) {
+//   let count = 0;
+//   function logCount() {
+//     count += 1;
+//     if (count > times) return;
+//     setTimeout(logCount, interval);
+//     console.log(count);
+//   }
+//   createPromise(delay, logCount);
 // }
+
+// function createPromise(delay, callback) {
+//   return new Promise((res) => {
+//     setTimeout(() => res(), delay);
+//   }).then(() => callback());
+// }
+// countWithDelay(1000, 5, 3000);
