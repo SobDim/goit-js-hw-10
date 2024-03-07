426dda7c8becfc9b8d703556e661c0bb0aa4b672
SobDim <101928077+SobDim@users.noreply.github.com>
2024-03-08 00:03:15 +0200

    +

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,23 +3,13 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+// Timer
 const dateTimePicker = document.querySelector('#datetime-picker');
+const display = document.querySelector('.timer');
 const startBtn = document.querySelector('[data-start]');
-const timer = document.querySelector('.timer');
-const msgOptions = {
-  message: 'Please choose a date in the future',
-  messageSize: '16px',
-  messageLineHeight: '24px',
-  messageColor: '#fff',
-  backgroundColor: '#EF4040',
-  progressBarColor: '#B51B1B',
-  theme: 'dark',
-  position: 'topRight',
-  class: 'message',
-};
-
 let diff;
-
+let selectedDate;
+// flatpickr options
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -27,44 +17,68 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (Date.now() > selectedDates[0]) {
-      ('Please choose a date in the future');
-      return;
+      startBtn.disabled = true;
+      return iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        iconUrl: '../img/error.svg',
+        backgroundColor: 'rgb(239, 64, 64)',
+        messageColor: 'rgb(255, 255, 255)',
+        titleColor: 'rgb(255, 255, 255)',
+        position: 'topRight',
+        close: 'rgb(255, 255, 255)',
+      });
     }
+    startBtn.disabled = false;
+    selectedDate = selectedDates[0];
+
+    countInterval();
   },
 };
 
 startBtn.disabled = true;
 
-// class Timer {
-//   constructor(callback, selectedTime) {
-//     this.tick = callback;
-//     this.intervalID = null;
-//     this.selectedTime = selectedTime;
-//   }
-//   start() {
-//     his.intervalID = setInterval(() => {
-//       const currentTime = Date.now();
-//       const diff = this.selectedTime - currentTime;
-//       renderTime(diff);
-//       if (diff < 500) this.stop();
-//       this.tick();
-//     }, 1000);
-//   }
-//   stop() {
-//     clearInterval(this.intervalID);
-//   }
-// }
+function countInterval() {
+  diff = selectedDate - Date.now();
+  const { days, hours, minutes, seconds } = convertMs(diff);
+  render(days, hours, minutes, seconds);
+}
 
-// const newTimer = new Timer();
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  dateTimePicker.disabled = true;
 
-// console.log(userSelectedDate);
-// options.onClose;
-// console.log(options.onClose);
+  const intervalId = setInterval(() => {
+    countInterval();
 
+    if (diff < 1000) {
+      clearInterval(intervalId);
+      dateTimePicker.disabled = false;
+    }
+  }, 1000);
+});
+
+// flatpickr run
 flatpickr(dateTimePicker, options);
-// console.log(userSelectedDate);
 
-//=========== export
+function render(days, hours, minutes, seconds) {
+  display.querySelector('[data-days]').textContent = String(days).padStart(
+    2,
+    '0'
+  );
+
+  display.querySelector('[data-hours]').textContent = String(hours).padStart(
+    2,
+    '0'
+  );
+  display.querySelector('[data-minutes]').textContent = String(
+    minutes
+  ).padStart(2, '0');
+  display.querySelector('[data-seconds]').textContent = String(
+    seconds
+  ).padStart(2, '0');
+}
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
